702d0be4b37509d7beac5cea7fa413ecbae29885
sarah <sarah.mohammadian95@gmail.com>
2021-08-27 01:10:06 +0430

    Just a game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const choices = ['rock', 'paper', 'scissors'];
+  
+
+
+  const handleClick = (value) => {
+    setUserChoice(value);
+    generateComputerChoice();
+    
+  }
+
+  useEffect(() => {
+    switch(userChoice + computerChoice) {
+      case 'scissorspaper':
+      case 'rockscissors':
+      case 'paperrock':
+        setResult('YOU WIN!')
+        break
+      case 'paperscissors':
+      case 'scissorsrock':
+      case 'rockpaper':
+        setResult('YOU LOSE!')
+        break
+      case 'rockrock':
+      case 'paperpaper':
+      case 'scissorsscissors':
+        setResult('ITS A DRAW!')
+        break
+    }
+  }, [computerChoice, userChoice])
+
+  const generateComputerChoice = () => {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(randomChoice);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User Choice Is:  {userChoice}</h1>
+      <h1>Computer Choice Is:  {computerChoice}</h1>
+
+      {choices.map((choice, index) => <button key={index} onClick={() => handleClick(choice)}>{choice}</button>)}
+      <h1>{result}</h1>
     </div>
   );
 }
